43b36f4b1cc16669fc7629b5c07ca41dcfd51a74
mukhlish <mukhlishanafi04@gmail.com>
2020-03-05 17:16:58 +0700

    featur-1 ready

--- App.js
+++ App.js
@@ -1,20 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import Greeting from './components/greeting';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <TextInput value="My Name" />
-      <Text>Welcome to CI/CD Course!</Text>
-    </View>
+    <Greeting />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
